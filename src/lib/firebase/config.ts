3f9fabfabc11598ff4469f00054a5739768fd5f0
Ankita Sharma <112687039+Ankita7572@users.noreply.github.com>
2025-06-20 13:00:29 +0530

    Update config.ts

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 export const firebaseConfig = {
-      apiKey: "AIzaSyCxkcvGS_WvONZRNky8ZzTpceO1t1lutEs",
-  authDomain: "pulse-fitness-bfac0.firebaseapp.com",
-  projectId: "pulse-fitness-bfac0",
-  storageBucket: "pulse-fitness-bfac0.firebasestorage.app",
-  messagingSenderId: "703836724917",
-  appId: "1:703836724917:web:8fbc2913d36f3a578aa8fc",
-  measurementId: "G-0QGZKG29KN"
+      apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId:process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId:process.env.NEXT_PUBLIC_MEASUREMENT_ID
  };
 
 // Initialize Firebase
